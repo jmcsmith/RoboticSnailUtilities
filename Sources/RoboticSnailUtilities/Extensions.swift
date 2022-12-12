754cd5c969dacc15a769b57b951e9d194e79dc8b
Joe Beaudoin <jmcsmith@gmail.com>
2022-12-12 13:53:40 -0500

    add day of week extension

--- Sources/RoboticSnailUtilities/Extensions.swift
+++ Sources/RoboticSnailUtilities/Extensions.swift
@@ -96,3 +96,11 @@ public extension Binding {
         })
     }
 }
+extension Date {
+    func dayOfWeek() -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: self).capitalized
+        // or use capitalized(with: locale) if you want
+    }
+}
