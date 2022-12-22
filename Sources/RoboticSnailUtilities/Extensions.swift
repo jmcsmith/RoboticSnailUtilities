f1e44187f7d64bc2c5de4314b2b37459553bfa2b
Joe Beaudoin <jmcsmith@gmail.com>
2022-12-22 15:00:02 -0500

    extension to Double for formatting seconds to different formats.

--- Sources/RoboticSnailUtilities/Extensions.swift
+++ Sources/RoboticSnailUtilities/Extensions.swift
@@ -125,3 +125,11 @@ public extension UIImage {
         return normalizedImage
     }
 }
+extension Double {
+  func asString(style: DateComponentsFormatter.UnitsStyle) -> String {
+    let formatter = DateComponentsFormatter()
+    formatter.allowedUnits = [.hour, .minute, .second, .nanosecond]
+    formatter.unitsStyle = style
+    return formatter.string(from: self) ?? ""
+  }
+}
